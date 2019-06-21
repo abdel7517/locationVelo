a0209c7083c1ff7999ca60d4d28b33c8a768ed90
worldweb <chabane.abdelkarim@worldweb.fr>
2019-06-21 20:50:55 +0200

    signature du canvas reussit mais avec décalage

--- canva.js
+++ canva.js
@@ -1,106 +1,100 @@
-let painting = false;
-let started = false;
-const canvas = $("#canvas");
-
-
-function mousedown() {
-        // Click souris enfoncé sur le canvas, je dessine :
-        $(this.canvas).mousedown(function (e) {
-            this.painting = true;
-
-            // Coordonnées de la souris :
-            this.cursorX = (e.pageX - this.offsetLeft);
-            this.cursorY = (e.pageY - this.offsetTop);
-
-        });
-
-    }
-
-
-if (painting)
+class Sign
 {
-    canvas.mousemove(function(e) {
-      var msg = "Handler for .mousemove() called at ";
-      msg += event.pageX + ", " + event.pageY;
-      $( "#a" ).append( "<div>" + msg + "</div>" );
-    });
-}
+    constructor()
+{
+   this.canvas = document.createElement('canvas');
 
+    $(this.canvas)
+       .attr('id', "canvas")
+       .text('unsupported browser')
+       .width("150px")
+       .height("100px")
+       .appendTo("form");
+this.context = this.canvas.getContext("2d");
+this.context.strokeStyle = "rgb(23, 145, 167)";
+this.context.lineCap = 'round';
+this.context.lineCap = 'round';
+this.cursorX, this.cursorY;
+this.painting = false;
+this.started = false;
+this.mousemove();
+this.mousedown();
+this.mouseup();
 
+};
 
+mousedown()
+        {
 
-
-class Sign {
-    constructor() {
-        // Variables :
-        this.color = "#000";
-        this.painting = false;
-        this.started = false;
-        this.width_brush = 1;
-        this.canvas = $("#canvas");
-        this.cursorX, this.cursorY;
-        this.context = this.canvas.getContext('2d');
-        // Trait arrondi :
-        this.context.lineJoin = 'round';
-        this.context.lineCap = 'round';
-        this.mousedown();
-        this.mouseup();
-        this.mousemove();
-        this.drawLine();
-    }
-    mousedown() {
-        // Click souris enfoncé sur le canvas, je dessine :
-        $(this.canvas).mousedown(function (e) {
+          $("#canvas").mousedown((e) =>
+          {
             this.painting = true;
-
             // Coordonnées de la souris :
-            this.cursorX = (e.pageX - this.offsetLeft);
-            this.cursorY = (e.pageY - this.offsetTop);
+            this.cursorX = e.pageX;
+            this.cursorY = e.pageY;
+          });
+         };
 
-        });
+mousemove()
+        {
+            $("#canvas").mousemove((e) =>
+            {
+             if (this.painting)
+             {
+                    this.cursorY = (e.pageY - canvas.offsetTop);
+                    this.cursorX = (e.pageX - canvas.offsetLeft);
+                    this.drawline();
+             }
 
-    }
+            });
 
+        };
 
+drawline()
+{
 
-    mousemove() {
-        // Mouvement de la souris sur le canvas :
-        $(this.canvas).mousemove(function (e) {
-            // Si je suis en train de dessiner (click souris enfoncé) :
-            if (this.painting) {
-                // Set Coordonnées de la souris :
-                this.cursorX = (e.pageX - this.offsetLeft) - 2; // 10 = décalage du curseur
-                this.cursorY = (e.pageY - this.offsetTop) - 2;
-                this.drawLine()
-            }
-        });
-    }
-    // Fonction qui dessine une ligne :
-    drawLine() {
-        // Si c'est le début, j'initialise
-        if (!this.started) {
-            // Je place mon curseur pour la première fois :
+    if (!this.started)
+    {
             this.context.beginPath();
             this.context.moveTo(this.cursorX, this.cursorY);
-            this.started = true;
-        }
-        // Sinon je dessine
-        else {
-            this.context.lineTo(this.cursorX, this.cursorY);
-            this.context.strokeStyle = this.color;
-            this.context.lineWidth = this.width_brush;
-            this.context.stroke();
-        }
+           console.log(this.cursorX);
+           console.log("a");
 
+            this.started = true;
+     }
+    else
+    {
+            this.context.lineTo(this.cursorX, this.cursorY);
+            this.context.stroke();
     }
 
-        mouseup() {
-        // Relachement du Click sur tout le document, j'arrête de dessiner :
-        $(this.canvas).mouseup(function () {
+};
+
+mouseup()
+{
+    $("#canvas").mouseup((e) =>
+        {
             this.painting = false;
             this.started = false;
-        });
-    }
+        })
+
 }
+
+
+};
+
+
+ new Sign();
+
+
+
+
+
+
+
+
+
+
+
 
 
